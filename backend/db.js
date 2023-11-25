832b7a8b6e489daae6747ba310d22e34e84e2425
love <yadavlove909@gmail.com>
2023-11-25 14:33:27 +0530

    done

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://0.0.0.0:27017/medico";
+const mongoURI = "mongodb://27017/medico";
 
 const connectToMongo = () =>{
     mongoose.connect(mongoURI);
--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-const connectToMongo = require('./db');
+const connectToMongo = require('../backend/db');
 const express = require('express')
 var cors = require('cors')
 
